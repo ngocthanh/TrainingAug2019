5326967a89473b79da2c321db0fa4f7c2194abed
Le Thanh <thanhln@tinhvan.com>
2019-08-23 09:18:50 +0700

    init project

---
+++ TrainingProject/TrainingProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrainingProject
+//
+//  Created by mac on 8/23/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
